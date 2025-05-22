4beeb84f07784ae8a9730e0328be5aeaaebf534a
Aykhan Shahsuvarov <aykhan.shahs@gmail.com>
2025-05-23 01:49:39 +0400

    Update 'users' list

--- pkg/generator/az/lists.go
+++ pkg/generator/az/lists.go
@@ -9,6 +9,11 @@ var users = generator.Users{
 	"did:plc:lc7j7xdq67gn7vc6vzmydfqk": false,
 	"did:plc:msian4dqa2rqalf3biilnf3m": false,
 	"did:plc:gtosalycg7snvodjhsze35jm": false,
+	"did:plc:i53e6y3liw2oaw4s6e6odw5m": false,
+	"did:plc:pvdqvmpkeermkhy7fezam473": false,
+	"did:plc:5vwjnzaibnwscbbcvkzhy57v": false,
+	"did:plc:6mfp3coadoobuvlg6w2avw6x": false,
+	
 
 	// Valid
 	"did:plc:jbt4qi6psd7rutwzedtecsq7": true,
@@ -18,4 +23,5 @@ var users = generator.Users{
 	"did:plc:jfdvklrs5n5qv7f25v6swc5h": true,
 	"did:plc:u5ez5w6qslh6advti4wyddba": true,
 	"did:plc:cs2cbzojm6hmx5lfxiuft3mq": true,
+	"did:plc:x7alwnnjygt2aqcwblhazko7": true,
 }
